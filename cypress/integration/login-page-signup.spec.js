9a541e867cf3f516b044af540012c0df1f3b117a
Qi Zeng <ivan_zq@foxmail.com>
2021-12-12 18:17:34 +0000

    E2E tests -Sign Up Test

--- cypress/integration/login-page-signup.spec.js
+++ cypress/integration/login-page-signup.spec.js
@@ -5,8 +5,10 @@ describe("Login Page ", () => {
 
     it("should successsfully log in", () => {
       cy.get('button').eq(8).click()
-      cy.get('input').eq(0).type('123@123.com')  
+      cy.get('a').click()
+      cy.get('input').eq(0).type('1111@123.com')  
       cy.get('input').eq(1).type('123456') 
+      cy.get('input').eq(2).type('123456') 
       cy.get('button').eq(9).click()
     })
   });
