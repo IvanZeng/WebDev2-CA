bc9c2aade37be7fcd8e4b5210cd04038227f5e19
Qi Zeng <ivan_zq@foxmail.com>
2021-12-13 07:04:48 +0000

    Route-based Code Splitting

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
-import React from "react";
+import {React,lazy,Suspense} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
-import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
-import MovieReviewPage from "./pages/movieReviewPage";
+// import HomePage from "./pages/homePage";
+// import MoviePage from "./pages/movieDetailsPage";
+// import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+// import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
-import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+// import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
-import MustwatchMoviesPage from "./pages/mustWatchMoviesPage"; 
-import TopRatedMoviesPage from "./pages/topRatedMoviesPage";
-import NowPlayingMoviesPage from "./pages/nowPlayingMoviesPage";
-import SimilarMoviesPage from "./pages/similarMoviesPage";
-import PopularPeoplePage from './pages/popularPeoplePage';
-import PeopleDetailsPage from './pages/peopleDetailsPage'
-import PopularMoviesPage from './pages/popularMoviesPage';
+// import AddMovieReviewPage from './pages/addMovieReviewPage'
+// import MustwatchMoviesPage from "./pages/mustWatchMoviesPage"; 
+// import TopRatedMoviesPage from "./pages/topRatedMoviesPage";
+// import NowPlayingMoviesPage from "./pages/nowPlayingMoviesPage";
+// import SimilarMoviesPage from "./pages/similarMoviesPage";
+// import PopularPeoplePage from './pages/popularPeoplePage';
+// import PeopleDetailsPage from './pages/peopleDetailsPage'
+// import PopularMoviesPage from './pages/popularMoviesPage';
 import SignUpPage from "./pages/signUpPage";
 import LogInPage from "./pages/logInPage";
 import "bootstrap/dist/css/bootstrap.min.css"
+
+
+const HomePage = lazy(() => import("./pages/homePage"));
+const MoviePage = lazy(() => import("./pages/movieDetailsPage"));
+const FavoriteMoviesPage = lazy(() => import("./pages/favoriteMoviesPage"));
+const MovieReviewPage = lazy(() => import("./pages/movieReviewPage"));
+const UpcomingMoviesPage = lazy(() => import("./pages/upcomingMoviesPage"));
+const AddMovieReviewPage = lazy(() => import("./pages/addMovieReviewPage"));
+const MustwatchMoviesPage = lazy(() => import("./pages/mustWatchMoviesPage"));
+const TopRatedMoviesPage = lazy(() => import("./pages/topRatedMoviesPage"));
+const NowPlayingMoviesPage = lazy(() => import("./pages/nowPlayingMoviesPage"));
+const SimilarMoviesPage = lazy(() => import("./pages/similarMoviesPage"));
+const PopularPeoplePage = lazy(() => import("./pages/popularPeoplePage")); 
+const PeopleDetailsPage = lazy(() => import("./pages/peopleDetailsPage"));  
+const PopularMoviesPage = lazy(() => import("./pages/popularMoviesPage")); 
+
+
 
 
 const queryClient = new QueryClient({
@@ -37,9 +54,12 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+      <Suspense fallback={<h1>Loading page</h1>}>
         <SiteHeader />
+        </Suspense>
         <MoviesContextProvider>
             {" "}
+            <Suspense fallback={<h1>Loading page</h1>}>
           <Switch>
         <Route exact path="/signUp" component={SignUpPage} />
         <Route exact path="/logIn" component={LogInPage} />
@@ -58,6 +78,7 @@ const App = () => {
         <Route exact path="/" component={HomePage} />
         <Redirect from="*" to="/" />
       </Switch>
+      </Suspense>
       </MoviesContextProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
