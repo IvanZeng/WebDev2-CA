f8470a867e951a709629adcdec76a10d33317b9e
Qi Zeng <ivan_zq@foxmail.com>
2021-12-13 07:13:43 +0000

    Route-based Code Splitting HomePage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,13 @@
-import React from "react";
-import PageTemplate from "../components/templateMovieListPage";
+import {React,lazy} from "react";
+// import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from 'react-query'
-import Spinner from '../components/spinner'
+// import Spinner from '../components/spinner'
 import {getMovies} from '../api/tmdb-api'
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+// import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+const PageTemplate = lazy(() => import("../components/templateMovieListPage"));
+const Spinner = lazy(() => import("../components/spinner"));
+const AddToFavoritesIcon = lazy(() => import("../components/cardIcons/addToFavorites"));
 
 const HomePage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
